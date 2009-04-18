44f1f9ae4755b15989cc86333d34da5c53a37836
Chris Marshall <chris@psinnovations>
2009-04-18 10:23:38 -0400

    Importing 1.0

---
+++ resources/page_modules.php
@@ -0,0 +1,42 @@
+<?php
+
+function page_titleJqueryCSS() {
+?>
+	<title>Presence Solution Innovatiosn - The Web, within reach</title>
+	<script type="text/javascript" src="jquery.js"></script>
+	
+<?php
+}
+
+function page_logo() {
+?>
+<div class='logo'>
+<a href='index.html'><img src='images/logo_for_web.png' height=200 border=0></a>
+</div>
+<?php
+}
+
+function page_sidemenu() {
+?>
+<div class='sideMenu'>
+<ul>
+<li><a href='philosophy.html' title='Philosophy'>the Web, within Reach</a></li>
+<li><a href='pricing.html' title='Pricing'>Doing Business</a></li>
+<li><a href='clients.html' title='Clients'>In Action</a></li>
+<li><a href='contact.html' title='Contacts'>Keep In Touch</a></li>
+</ul>
+</div> <!-- end sideMenu -->
+<?php
+}
+
+function page_footer() {
+?>
+	<div style='clear:both'>&nbsp;</div>
+	<div class='footer'>
+	2008 All Rights Reserved Presence Solution Innovations
+	</div> <!-- end footer -->
+<?php
+}
+
+
+?>
